0956e58c008c242bec2cdfa91975dc0eb964cae3
hassan-yaqoob <56113635+hassan-yaqoob@users.noreply.github.com>
2020-10-22 23:58:04 +0100

    Register & Login pages with back end code

--- webapp/Application/app/models.py
+++ webapp/Application/app/models.py
@@ -2,6 +2,21 @@
 Definition of models.
 """
 
-from django.db import models
+#from django.db import models
+from djongo import models
 
 # Create your models here.
+class User_profile(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    confirm_password = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=30)
+    zipcode = models.CharField(max_length=10)
+    dob=models.DateField()
+    gender=models.CharField(max_length=10)
+    height=models.DecimalField(max_digits=10, decimal_places=3)
+    weight=models.DecimalField(max_digits=10, decimal_places=3)
