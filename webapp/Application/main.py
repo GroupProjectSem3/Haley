f0ec9f50fa6b429f0ebcb58e67c1e7c96d22d0d9
Geetika Agarwal <64841134+GeetAgar@users.noreply.github.com>
2020-11-16 10:09:45 +0530

    Changes in main.py filr

--- webapp/Application/main.py
+++ webapp/Application/main.py
@@ -1,4 +1,4 @@
-from mysite.wsgi import application
+from python_webapp_django.wsgi import application
 
 # App Engine by default looks for a main.py file at the root of the app
 # directory with a WSGI-compatible object called app.
